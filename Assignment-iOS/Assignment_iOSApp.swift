3a3f0cf51d79d0f74e891030c15c0fb7ce3587d5
aditya choubisa <adityachoubisa02@gmail.com>
2024-11-11 01:44:05 +0530

    Initial Commit

---
+++ Assignment-iOS/Assignment_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_iOSApp.swift
+//  Assignment-iOS
+//
+//  Created by APPLE on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assignment-iOS
+//
+//  Created by APPLE on 11/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
